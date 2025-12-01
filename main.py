c3d91b42ab333e60c5db53d09b991c3d25b47176
AlexMurray0396 <the.primeagen@aol.com>
2025-12-01 17:26:56 +0000

    move player

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while 1==1:
         log_state()
@@ -19,12 +20,11 @@ def main():
                 return
 
         screen.fill("black")
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
         player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt += clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
